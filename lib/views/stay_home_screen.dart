9026d93e9d15ffe08f83855f47ee037347a9d230
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-15 19:13:37 +0300

    init

--- lib/views/stay_home_screen.dart
+++ lib/views/stay_home_screen.dart
@@ -1,12 +1,20 @@
+import 'package:covid19_app/models/countries_data_model.dart';
+import 'package:covid19_app/services/countries_data_service.dart';
 import 'package:flutter/material.dart';
 
 class StayHomeScreen extends StatelessWidget {
-  const StayHomeScreen({Key? key}) : super(key: key);
-
+   StayHomeScreen({Key? key}) : super(key: key);
+  CountriesModel _countriesModel = CountriesModel();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: ElevatedButton(onPressed: () async {
+          _countriesModel = await GetCountriesData().getData("Turkey");
+          print(_countriesModel.country);
+          print(_countriesModel.totalCases);
+        }, child: Text("test")),
+      ),
     );
   }
 }
