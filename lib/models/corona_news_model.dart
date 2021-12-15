8af852b296cb6124a58f59d332ae2bd0a4e66611
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-15 20:50:23 +0300

    models added

--- lib/models/corona_news_model.dart
+++ lib/models/corona_news_model.dart
@@ -1,3 +1,31 @@
 class CoronaNewsModel {
-  
+  final String? url;
+  final String? description;
+  final String? image;
+  final String? name;
+  final String? source;
+
+  CoronaNewsModel({
+    this.url,
+    this.description,
+    this.image,
+    this.name,
+    this.source,
+  });
+
+  factory CoronaNewsModel.fromJson(Map<String, dynamic> json) {
+    final url = json["result"][0]["url"].toString();
+    final description = json["result"][0]["description"].toString();
+    final image = json["result"][0]["image"].toString();
+    final name = json["result"][0]["name"].toString();
+    final source = json["result"][0]["source"].toString();
+
+    return CoronaNewsModel(
+      url: url,
+      description: description,
+      image: image,
+      name: name,
+      source: source,
+    );
+  }
 }
--- lib/models/total_data_model.dart
+++ lib/models/total_data_model.dart
@@ -1,3 +1,23 @@
 class TotalDataModel {
-  
+  final String? totalDeaths;
+  final String? totalCases;
+  final String? totalRecovered;
+
+  TotalDataModel({
+    this.totalDeaths,
+    this.totalCases,
+    this.totalRecovered,
+  });
+
+  factory TotalDataModel.fromJson(Map<String, dynamic> json) {
+    final totalDeaths = json["result"]["totalDeaths"].toString();
+    final totalCases = json["result"]["totalCases"].toString();
+    final totalRecovered = json["result"]["totalRecovered"].toString();
+
+    return TotalDataModel(
+      totalDeaths: totalDeaths,
+      totalCases: totalCases,
+      totalRecovered: totalRecovered,
+    );
+  }
 }
