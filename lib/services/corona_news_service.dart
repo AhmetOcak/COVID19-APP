8ed8936eb7ae70aca1c314bf8601db3ca80c9ede
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-16 23:21:11 +0300

    reorganized

--- lib/services/corona_news_service.dart
+++ lib/services/corona_news_service.dart
@@ -4,15 +4,20 @@ import 'package:covid19_app/models/corona_news_model.dart';
 import 'package:http/http.dart' as http;
 
 class GetCoronaNews {
-  Future<CoronaNewsModel> getData() async {
+  Future<Null> getData() async {
     Uri url = Uri.parse("https://api.collectapi.com/corona/coronaNews");
 
-    var response = await http.post(url, headers: {
-      "content-type": "application/json",
-      "authorization": "apikey 2ehyKijlw4TOgwHb5zGL11:7wyAgBY84SwkjUDPGVaZmG",
-    });
+    var response = await http.post(
+      url,
+      headers: {
+        "content-type": "application/json",
+        "authorization": "apikey 2ehyKijlw4TOgwHb5zGL11:7wyAgBY84SwkjUDPGVaZmG",
+      },
+    );
+    
+     var json = jsonDecode(response.body)["result"] as List; 
+     List<CoronaNewsModel> newsList = json.map((e) => CoronaNewsModel.fromJson(e)).toList();
+     print(newsList);
 
-    final json = jsonDecode(response.body);
-    return CoronaNewsModel.fromJson(json);
   }
 }
