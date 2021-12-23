efbdf9d664a7083c4e4be294244a3378aa5f207f
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-23 19:37:40 +0300

    curved navbar ayarları yapıldı

---
+++ lib/constants/curved_navbar_items.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+final items = <Widget>[
+  Icon(Icons.home, size: 30,),
+  Icon(Icons.home, size: 30,),
+  Icon(Icons.home, size: 30,),
+  Icon(Icons.home, size: 30,),
+  Icon(Icons.home, size: 30,),
+];
+
---
+++ lib/constants/screens.dart
@@ -0,0 +1,13 @@
+import 'package:covid19_app/views/continent_data_screen.dart';
+import 'package:covid19_app/views/corona_news_screen.dart';
+import 'package:covid19_app/views/country_data_screen.dart';
+import 'package:covid19_app/views/stay_home_screen.dart';
+import 'package:covid19_app/views/total_data_screen.dart';
+
+final screens = [
+  NewsScreen(),
+  TotalDataScreen(),
+  StayHomeScreen(),
+  CountryDataScreen(),
+  ContinentDataScreen()
+];
