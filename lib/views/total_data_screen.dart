2ecac457decd013f4f702eb327bd81cfc1dc19fe
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-01-24 14:09:25 +0300

    total data can get now

--- lib/views/total_data_screen.dart
+++ lib/views/total_data_screen.dart
@@ -1,4 +1,9 @@
+import 'package:covid19_app/components/data_card.dart';
+import 'package:covid19_app/constants/colors.dart';
+import 'package:covid19_app/models/total_data_model.dart';
+import 'package:covid19_app/services/total_data_service.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class TotalDataScreen extends StatefulWidget {
   TotalDataScreen({Key? key}) : super(key: key);
@@ -8,10 +13,58 @@ class TotalDataScreen extends StatefulWidget {
 }
 
 class _TotalDataScreenState extends State<TotalDataScreen> {
+
+  GetTotalData _totalData = GetTotalData();
+  TotalDataModel _dataModel = TotalDataModel();
+
+  Future<TotalDataModel> getTotalData() async {
+    TotalDataModel dataModel = await _totalData.getData();
+    _dataModel = dataModel;
+    return _dataModel;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTotalData().then((value) => setState(() {}));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('total'),
+    return Scaffold(
+      backgroundColor: mainColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  bottom: 50,
+                ),
+                child: FaIcon(
+                  FontAwesomeIcons.skull,
+                  size: MediaQuery.of(context).size.width / 2,
+                  color: skullColor,
+                ),
+              ),
+              TotalCard(
+                text: "Total Deaths",
+                data: _dataModel.totalDeaths.toString(),
+              ),
+              TotalCard(
+                text: "Total Cases",
+                data: _dataModel.totalCases.toString(),
+              ),
+              TotalCard(
+                text: "Total Recovered",
+                data: _dataModel.totalRecovered.toString(),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
