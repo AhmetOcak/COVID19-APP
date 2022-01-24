065de4c8606efe0983abf7faf93f0c3f9a08b627
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-01-24 13:26:29 +0300

    icons edited

--- lib/constants/curved_navbar_items.dart
+++ lib/constants/curved_navbar_items.dart
@@ -1,12 +1,33 @@
+import 'package:covid19_app/constants/colors.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-final items = <Widget>[
-  Icon(Icons.home, size: 30,),
-  Icon(Icons.home, size: 30,),
-  Icon(Icons.home, size: 30,),
-  Icon(Icons.home, size: 30,),
-  Icon(Icons.home, size: 30,),
+const items = <Widget>[
+  FaIcon(
+    FontAwesomeIcons.newspaper,
+    size: 30,
+    color: iconColor,
+  ),
+  FaIcon(
+    FontAwesomeIcons.globe,
+    size: 30,
+    color: iconColor,
+  ),
+  FaIcon(
+    FontAwesomeIcons.home,
+    size: 30,
+    color: iconColor,
+  ),
+  FaIcon(
+    FontAwesomeIcons.flag,
+    size: 30,
+    color: iconColor,
+  ),
+  FaIcon(
+    FontAwesomeIcons.canadianMapleLeaf,
+    size: 30,
+    color: iconColor,
+  ),
 ];
-
