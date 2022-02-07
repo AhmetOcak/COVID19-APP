4381ceed8ba95ead92b66e7913eae6ad2fe1b9e3
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-02-07 20:50:56 +0300

    country data end

---
+++ lib/components/country_data_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class CountryDataCard extends StatelessWidget {
+  CountryDataCard({
+    required this.country,
+    required this.totalCases,
+    required this.newCases,
+    required this.totalDeaths,
+    required this.activeCases,
+    required this.newDeaths,
+    required this.totalRecovered,
+    Key? key,
+  }) : super(key: key);
+
+  final String country;
+  final String totalCases;
+  final String newCases;
+  final String totalDeaths;
+  final String newDeaths;
+  final String totalRecovered;
+  final String activeCases;
+
+  TextStyle style = const TextStyle(
+    fontSize: 24,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white70,
+      child: Column(
+        children: [
+          Text(
+            country,
+            style: const TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                "Total Cases: $totalCases",
+                style: style,
+              ),
+              Text(
+                "Active Cases: $activeCases",
+                style: style,
+              ),
+              Text(
+                "New Cases: $newCases",
+                style: style,
+              ),
+              Text(
+                "Total Deaths: $totalDeaths",
+                style: style,
+              ),
+              Text(
+                "Total Recovered: $totalRecovered",
+                style: style,
+              ),
+              Text(
+                "New Deaths: $newDeaths",
+                style: style,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/country_data_screen.dart
+++ lib/views/country_data_screen.dart
@@ -1,4 +1,7 @@
+import 'package:covid19_app/components/country_data_card.dart';
 import 'package:covid19_app/constants/colors.dart';
+import 'package:covid19_app/models/countries_data_model.dart';
+import 'package:covid19_app/services/countries_data_service.dart';
 import 'package:flutter/material.dart';
 
 class CountryDataScreen extends StatefulWidget {
@@ -9,12 +12,63 @@ class CountryDataScreen extends StatefulWidget {
 }
 
 class _CountryDataScreenState extends State<CountryDataScreen> {
+  CountriesModel _countriesModel = new CountriesModel();
+  GetCountriesData _getCountriesData = new GetCountriesData();
+
+  int listLength = 0;
+
+  Future<CountriesModel> getCountriesData() async {
+    CountriesModel countriesModel = await _getCountriesData.loadData();
+    _countriesModel = countriesModel;
+    return _countriesModel;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCountriesData().then((value) => setState(() {
+          listLength = _countriesModel.result!.length;
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: mainColor,
-      body: Column(
-        
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                top: 10,
+              ),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: ListView.builder(
+                itemCount: listLength,
+                itemBuilder: (BuildContext context, int index) {
+                  return CountryDataCard(
+                    country: _countriesModel.result![index].country.toString(),
+                    activeCases:
+                        _countriesModel.result![index].activeCases.toString(),
+                    newCases:
+                        _countriesModel.result![index].newCases.toString(),
+                    newDeaths:
+                        _countriesModel.result![index].newDeaths.toString(),
+                    totalCases:
+                        _countriesModel.result![index].totalCases.toString(),
+                    totalDeaths:
+                        _countriesModel.result![index].totalDeaths.toString(),
+                    totalRecovered: _countriesModel
+                        .result![index].totalRecovered
+                        .toString(),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
