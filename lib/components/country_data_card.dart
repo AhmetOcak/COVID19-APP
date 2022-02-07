bbd0f329929da42882219c912ee2f5ff6054e5d8
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-02-07 21:55:25 +0300

    reorganized

--- lib/components/country_data_card.dart
+++ lib/components/country_data_card.dart
@@ -27,45 +27,52 @@ class CountryDataCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8.0,),
       color: Colors.white70,
-      child: Column(
-        children: [
-          Text(
-            country,
-            style: const TextStyle(
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              country,
+              style: const TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Column(
-            children: [
-              Text(
-                "Total Cases: $totalCases",
-                style: style,
-              ),
-              Text(
-                "Active Cases: $activeCases",
-                style: style,
-              ),
-              Text(
-                "New Cases: $newCases",
-                style: style,
-              ),
-              Text(
-                "Total Deaths: $totalDeaths",
-                style: style,
-              ),
-              Text(
-                "Total Recovered: $totalRecovered",
-                style: style,
-              ),
-              Text(
-                "New Deaths: $newDeaths",
-                style: style,
-              ),
-            ],
-          ),
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+            Column(
+              children: [
+                Text(
+                  "Total Cases: $totalCases",
+                  style: style,
+                ),
+                Text(
+                  "Active Cases: $activeCases",
+                  style: style,
+                ),
+                Text(
+                  "New Cases: $newCases",
+                  style: style,
+                ),
+                Text(
+                  "Total Deaths: $totalDeaths",
+                  style: style,
+                ),
+                Text(
+                  "Total Recovered: $totalRecovered",
+                  style: style,
+                ),
+                Text(
+                  "New Deaths: $newDeaths",
+                  style: style,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
