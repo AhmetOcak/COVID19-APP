9e1d6866a42fe1d9df95f2bdf38d893a2f5b0400
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-02-08 13:26:15 +0300

    spinkit color created

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color mainColor = Colors.blue;
 const Color iconColor = Colors.blue;
 const Color skullColor = Colors.white;
+const Color spinKitColor = Colors.white;
